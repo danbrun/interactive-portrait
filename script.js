2a08038537b76264b78bca9faf2deb6a4c218f4d
Dan Brunwasser <me@danb.link>
2020-02-02 23:13:25 -0500

    Code cleanup and finished game logic

--- script.js
+++ script.js
@@ -1,104 +1,156 @@
+const CANVAS_SIZE = 1000;
+const MAX_DEPTH = 6;
+
+// Function to convert RGBA colors to CSS string.
+function colorToString(r, g, b, a) {
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function screenToCanvas(canvas, x, y) {
+    var scale = CANVAS_SIZE / canvas.scrollWidth;
+
+    return [
+        (x - canvas.offsetLeft) * scale,
+        (y - canvas.offsetTop) * scale,
+    ];
+}
+
 class QuadTree {
-	constructor(context, x1, y1, x2, y2, depth) {
-    	this.context = context;
-    	this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.isSplit = false;
+
+    constructor(source_context, dest_context, x, y, size, depth) {
+        this.source = source_context;
+        this.dest = dest_context;
+
+        this.x = x;
+        this.y = y;
+        this.size = size;
         this.depth = depth;
-        this.tree1 = null;
-        this.tree2 = null;
-        this.tree3 = null;
-        this.tree4 = null;
-    }
-    
-    render(){
-        if(this.isSplit){
-            this.tree1.render();
-            this.tree2.render();
-            this.tree3.render();
-            this.tree4.render();
-        }
 
-        var colAvg = getAvg();
-        
-    	context.fillStype = "rgba(" + colAvg[0] + "," + colAvg[1] + "," + colAvg[2] + "," + 1 + ")";
-        context.fillRect(x,y,size,size);
+        this.children = null;
     }
 
-    getAvg(){
-        var avg = new Array(3); //representation of average colors as integer array
+    average() {
+        var mid = Math.floor(this.size / 2);
 
-        var num_pixels_scanned = 0;
+        // Get the pixel color of the middle pixel.
+        return this.source.getImageData(this.x + mid, this.y + mid, 1, 1).data;
+    }
 
-        for(var x = this.x1; x < this.x2; x += 4){
-            for(var y = this.y1; y < this.y2; y += 4){
-                //FIX THIS I DON'T KNOW HOW TO DO THIS AHDHASKLJDLJKAHBKVBL
-                this.context.get_color()
-                //add the r g b values of the color at the point [x,y] to the average
-                //avg[0] += r;
-                //avg[1] += g;
-                //avg[2] += b;
+    render() {
+        if (this.children) {
+            // If there are children, render them instead.
+            for (var child of this.children) {
+                child.render();
             }
-        }
-        
-        avg[0] /= num_pixels_scanned;
-        avg[1] /= num_pixels_scanned;
-        avg[2] /= num_pixels_scanned;
-
-        return avg;
-    }
-
-    onclick(){
-        var x1_5 = avg(x1,x2);
-        var y1_5 = avg(y1,y2);
-
-
-        if(mouseX >= x1 && mouseX <= x2 && mouseY >= y1 && mouseY <= y2){
-            if(this.isSplit){
-                //check which quadrant the mouse is in
-                if(mouseX < x1_5 && mouseY < y1_5){
-                    this.tree1.onclick();
-                }
-                if(mouseX > x1_5 && mouseY < y1_5){
-                    this.tree2.onclick();
-                }
-                if(mouseX < x1_5 && mouseY > y1_5){
-                    this.tree3.onclick();
-                }
-                if(mouseX > x1_5 && mouseY > y1_5){
-                    this.tree4.onclick();
-                }
-            } else { //if quadrant is not split
-                this.splitSelf();
-            }
+        } else if (this.depth < MAX_DEPTH) {
+            // If below the max depth, render the average color.
+            this.dest.fillStyle = colorToString(...this.average());
+            this.dest.fillRect(this.x, this.y, this.size, this.size);
         } else {
-            //THROW ERROR of some flavor
+            // Otherwise render the original image in this region.
+            this.dest.drawImage(
+                this.source.canvas,
+                this.x, this.y, this.size, this.size,
+                this.x, this.y, this.size, this.size,
+            );
         }
-        
     }
 
-    splitSelf(){
-        var x1_5 = avg(x1,x2);
-        var y1_5 = avg(y1,y2);
-
-        //create four child trees
-        this.tree1 = new QuadTree(this.context,this.x1,this.y1,x1_5,y1_5,this.depth+1);
-        this.tree2 = new QuadTree(this.context,x1_5,this.y1,this.x2,y1_5,this.depth+1);
-        this.tree3 = new QuadTree(this.context,this.x1,y1_5,x1_5,y2,this.depth+1);
-        this.tree4 = new QuadTree(this.context,x1_5,y1_5,this.x2,this.y2,this.depth+1);
-
-        this.isSplit = true;
+    trigger(x, y) {
+        // If triggerred within this square.
+        if (this.x <= x && x < this.x + this.size) {
+            if (this.y <= y && y < this.y + this.size) {
+                if (this.children) {
+                    // Trigger children if they exist.
+                    for (var child of this.children) {
+                        child.trigger(x, y);
+                    }
+                } else {
+                    // Split self if at the end.
+                    this.split();
+                }
+            }
+        }
     }
+
+    split() {
+        var mid = Math.floor(this.size / 2);
+
+        if (this.depth < MAX_DEPTH && mid >= 1) {
+            // Add the children to the tree.
+            this.children = [
+                new QuadTree(
+                    this.source, this.dest,
+                    this.x, this.y,
+                    mid, this.depth + 1,
+                ),
+                new QuadTree(
+                    this.source, this.dest,
+                    this.x + mid, this.y,
+                    this.size - mid, this.depth + 1,
+                ),
+                new QuadTree(
+                    this.source, this.dest,
+                    this.x, this.y + mid,
+                    this.size - mid, this.depth + 1,
+                ),
+                new QuadTree(
+                    this.source, this.dest,
+                    this.x + mid, this.y + mid,
+                    this.size - mid, this.depth + 1,
+                ),
+            ];
+
+            // Rerender this node.
+            this.render();
+        }
+    }
+
 }
 
-var cvs = document.getElementById("Image");
+// Create an image element to load the picture.
+var image = new Image();
 
-var startingBoi = new QuadTree(canvas.getContext("2d"),0,0,cvs.width,cvs.height,cvs.width,cvs.height,0);
+// Get the original image canvas and context.
+var src_canvas = document.createElement('canvas');
+var src_context = src_canvas.getContext('2d');
+// Update the rendering size.
+src_canvas.width = CANVAS_SIZE;
+src_canvas.height = CANVAS_SIZE;
 
-startingBoi.render();
+// Get the game canvas and context.
+var dest_canvas = document.getElementById('canvas');
+var dest_context = dest_canvas.getContext('2d');
+// Update the rendering size.
+dest_canvas.width = CANVAS_SIZE;
+dest_canvas.height = CANVAS_SIZE;
 
-function onclick(){
-    startingBoi.onclick();
-}
+// Begin game after image is loaded.
+image.addEventListener('load', function () {
+    // Draw the image in the source canvas for pixel manipulation.
+    src_context.drawImage(image, 0, 0, 1000, 1000);
+
+    // Create a tree root.
+    var root = new QuadTree(src_context, dest_context, 0, 0, CANVAS_SIZE, 0);
+    // Render initial state.
+    root.render();
+
+    // Bind mouse to trigger tree.
+    dest_canvas.addEventListener('mousemove', function (event) {
+        root.trigger(
+            ...screenToCanvas(dest_canvas, event.clientX, event.clientY),
+        );
+    });
+
+    // Bind touch to trigger tree.
+    dest_canvas.addEventListener('touchmove', function (event) {
+        // root.trigger(...screenToCanvas(dest_canvas, ))
+        root.trigger(
+            ...screenToCanvas(dest_canvas, event.touches[0].clientX, event.touches[0].clientY)
+        )
+        event.preventDefault();
+    });
+});
+
+// Start loading the game image.
+image.src = 'test.jpg';
